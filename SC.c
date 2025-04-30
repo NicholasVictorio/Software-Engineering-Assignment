c0e339b90cb0f353aa8e197ae3d2d74bcc7427f7
NicholasVictorio <157617291+NicholasVictorio@users.noreply.github.com>
2025-04-30 23:00:29 +0700

    Update SC.c

--- SC.c
+++ SC.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Ini adalah versi pertama dari program sederhana dengan bahasa pemograman C.\n");
+    printf("Ini adalah versi kedua dari program sederhana dengan bahasa pemograman C.\n");
     return 0;
 }
